3e4a154e7fdbad2412998c5268503ee44214c1e8
Samarth B S <75886861+samarthbsss@users.noreply.github.com>
2024-03-07 12:03:15 +0000

    added font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+      heading: ["Shojumaru", "cursive"],
+    },
   },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
-}
-
+  plugins: [require("tailwindcss"), require("autoprefixer")],
+};
